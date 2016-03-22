6c74e26c09bd2b49464a6c1901aed4bcfe4fd236
a1224satoshi <satoshi4221@icloud.com>
2016-03-22 09:04:30 +0000

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :limit_user, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
@@ -19,11 +21,41 @@ class UsersController < ApplicationController
       render 'new'
     end
    end
-
+   
+   def edit
+     @user = User.find(params[:id])
+   end
+   
+   def update
+     @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+       flash[:success] = "Your Account was updated!"
+       redirect_to @user
+      else
+        render 'edit'
+      end
+   end
+   
+   def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+   
+   def limit_user
+     @user = User.find(params[:id])
+      unless @user == current_user
+      redirect_to root_url
+    end
+      
+        
+      end
+     
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :region, :profile)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20160320070219) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "region"
+    t.string   "profile"
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
